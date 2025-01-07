9dd9642966ee7e316a8add2ee23639aea230f93d
Ganesh Gunasegaran <me@itsgg.com>
2025-01-07 15:46:44 +0530

    Cleanup

--- speech_to_text.py
+++ speech_to_text.py
@@ -8,8 +8,7 @@ from openai import OpenAI
 client = OpenAI()
 client.api_key = os.getenv("OPENAI_API_KEY")
 
-transcript = client.audio.transcriptions.create(
-    model="whisper-1", file=open("output.mp3", "rb")
-)
+with open("output.mp3", "rb") as audio_file:
+    transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
 
-print(transcript)
+print(transcript.text)
